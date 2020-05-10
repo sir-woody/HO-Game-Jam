adbd992ad51f9eb72383f860eef602c4b4273a2a
sirwoody <sir.woody@theknightsofunity.com>
2020-05-10 15:36:23 +0200

    Fixed raycasting characters in tent.

--- Assets/Scripts/Seat.cs
+++ Assets/Scripts/Seat.cs
@@ -11,19 +11,21 @@ public class Seat : MonoBehaviour
     [SerializeField]
     private Image image = null;
     [SerializeField]
+    private Image maskImage = null;
+    [SerializeField]
     private Button button = null;
 
-    private Character character = null;
-    public Character Character => character;
+    public Character Character { get; private set; } = null;
 
     public void SetCharacter(Character character)
     {
-        this.character = character;
+        this.Character = character;
 
         Sprite spriteIdle = character.GetSprite(Character.SpriteType.Idle);
         Sprite spriteHoover = character.GetSprite(Character.SpriteType.Hoover);
 
         image.sprite = spriteIdle;
+        maskImage.sprite = spriteHoover;
         SpriteState spriteState = button.spriteState;
         /// Replacing pressedSprite and selectedSprite with null for better visual effect
         //spriteState.selectedSprite = spriteHoover;
---
+++ Assets/Scripts/Utils/RectTransformFitter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class RectTransformFitter
+{
+    [MenuItem("CONTEXT/RectTransform/Set Native Position With Anchors", false, 0)]
+    private static void SetNativePosition()
+    {
+        foreach (UnityEngine.Object obj in Selection.objects)
+        {
+            GameObject transform = obj as GameObject;
+            if (transform == null)
+            {
+                continue;
+            }
+            RectTransform rectTransform = transform.GetComponent<RectTransform>();
+            if (rectTransform == null)
+            {
+                continue;
+            }
+            if (rectTransform.parent == null)
+            {
+                continue;
+            }
+            RectTransform parent = rectTransform.parent.GetComponent<RectTransform>();
+            if (parent == null)
+            {
+                continue;
+            }
+
+            Vector2 min = new Vector2(
+                InverseLerpUnclamped(-parent.rect.width * parent.pivot.x, parent.rect.width * (1 - parent.pivot.x), AnchoredToCenter(rectTransform).x - rectTransform.rect.width * 0.5f),
+                InverseLerpUnclamped(-parent.rect.height * parent.pivot.y, parent.rect.height * (1 - parent.pivot.y), AnchoredToCenter(rectTransform).y - rectTransform.rect.height * 0.5f));
+            Vector2 max = new Vector2(
+                InverseLerpUnclamped(-parent.rect.width * parent.pivot.x, parent.rect.width * (1 - parent.pivot.x), AnchoredToCenter(rectTransform).x + rectTransform.rect.width * 0.5f),
+                InverseLerpUnclamped(-parent.rect.height * parent.pivot.y, parent.rect.height * (1 - parent.pivot.y), AnchoredToCenter(rectTransform).y + rectTransform.rect.height * 0.5f));
+
+            Undo.RecordObject(rectTransform, "Fit Anchors");
+            rectTransform.anchorMin = min;
+            rectTransform.anchorMax = max;
+            rectTransform.sizeDelta = Vector2.zero;
+            rectTransform.anchoredPosition = Vector2.zero;
+        }
+    }
+
+    private static Vector2 AnchoredToCenter(RectTransform rectTransform)
+    {
+        return (Vector2)rectTransform.localPosition - (rectTransform.pivot - Vector2.one * 0.5f) * rectTransform.rect.size;
+    }
+    private static float InverseLerpUnclamped(float a, float b, float value)
+    {
+        return (value - a) / (b - a);
+    }
+
+}
